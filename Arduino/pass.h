e50c148c0f3e96ed912fde6afe19753bf1c4e7bb
Piotr Lechowicz <piotr.lechowicz@gmail.com>
2015-08-26 17:05:59 +0200

    del

--- Arduino/pass.h
+++
@@ -1,5 +0,0 @@
-#define WLAN_SSID       "myNetwork"   // cannot be longer than 32 characters!
-#define WLAN_PASS       "myPassword"
-// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
-#define WLAN_SECURITY   WLAN_SEC_WPA2
-
